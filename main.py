49c78f1462cbdba26f3564b1169e2f8fded9e54b
Oleksii <alekseishevchenko8@gmail.com>
2023-12-24 19:38:01 +0200

    test_complete

--- main.py
+++ main.py
@@ -3,54 +3,56 @@ from datetime import datetime
 
 class Field:
     def __init__(self, value):
-        self.value = value
+        self.value = self.is_valid(value)
+
+    def is_valid(self, value):
+        return value 
 
     def __str__(self):
         return str(self.value)
 
-    
 class Name(Field):
     pass
 
-
 class Phone(Field):
-    def __init__(self, value):
-        super().__init__(self.validate_phone(value))
-
-    @staticmethod
-    def validate_phone(phone):
-        if not phone.isdigit() or len(phone) != 10:
+    def is_valid(self, value):
+        if not value.isdigit() or len(value) != 10:
             raise ValueError("Invalid phone number format. Please enter a 10-digit number.")
-        return phone
+        return value
 
+    @property
+    def value(self):
+        return self._value
 
+    @value.setter
+    def value(self, new_value):
+        self._value = self.is_valid(new_value)
 
 class Birthday(Field):
 
-    def __init__(self, value):
-        super().__init__(value)
-
-    @staticmethod
-    def validate_birthday(self, value):
+    def is_valid(self, value):
         try:
             datetime.strptime(value, "%d-%m-%Y")
-            return True
-        
         except ValueError:
+            raise ValueError("Invalid birthday format. Please use DD-MM-YYYY.")
+        return value
 
-            return False
+    @property
+    def value(self):
+        return self._value
 
-    
+    @value.setter
+    def value(self, new_value):
+        self._value = self.is_valid(new_value)
+
 class Record:
-    
-    
-    def __init__(self, name, birthday = None):
+    def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
         self.birthday = None
 
-        if birthday and Birthday.validate_birthday(birthday):
-            self.birthday_date = Birthday(birthday)
+        if birthday:
+            self.birthday = Birthday(birthday)
 
     def add_phone(self, phone):
         phone_obj = Phone(phone)
@@ -81,23 +83,24 @@ class Record:
     def days_to_birthday(self):
         if self.birthday:
             today = datetime.now()
-            if today > self.birthday:
-                next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            birthday_date = datetime.strptime(self.birthday.value, "%d-%m-%Y")
+            if today > birthday_date:
+                next_birthday = datetime(today.year + 1, birthday_date.month, birthday_date.day)
             else:
-                next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+                next_birthday = datetime(today.year + 1, birthday_date.month, birthday_date.day)
 
             return (next_birthday - today).days
         
         return None
 
-
-
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(contact.value for contact in self.phones)}"
+        birthday_str = f', birthday: {self.birthday.value}' if self.birthday else ''
+        return f"Contact name: {self.name.value}, {birthday_str} phones: {'; '.join(contact.value for contact in self.phones)}"
 
-    
 class AddressBook(UserDict):
-    
+    def __init__(self):
+        self.data = {}
+
     def add_record(self, record):
         self.data[record.name.value] = record
 
@@ -116,27 +119,5 @@ class AddressBook(UserDict):
     def iterator(self, n):
         for i in range(0, len(self.data), n):
             yield list(self.data.values())[i:i + n]
-        
 
-book = AddressBook()
 
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555")
-book.add_record(john_record)
-
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-
-for name, record in book.data.items():
-    print(record)
-
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-print(john)
-
-found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")
-
-book.delete("Jane")
